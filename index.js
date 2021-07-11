31db985a0c2156da03421454c7b31aae05e29f2f
Bridget Louise McCavanagh <blmccavanagh@gmail.com>
2021-07-12 01:05:18 +0800

    Bug in index.js file fixed.

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+const generateHTML = require('./utils/generateHTML');
 const createTeamMember = require('./src/createTeamMember')
 
-createTeamMember();
+const fs = require('fs');
+const inquirer = require('inquirer');
+
+// TODO: Create a function to initialize app
+function init() {
+    inquirer.prompt(questions).then((data) => {
+        writeToFile("index.html", generateHTML(data));
+    });
+}
+
+// Function call to initialize app
+init(); 
+createTeamMember(); 
